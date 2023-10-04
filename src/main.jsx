d90127e4be1e62b104f9939ecb393e7af0633efb
Thzzao <thiago.santos1160@gmail.com>
2023-10-04 19:44:26 -0300

    feat: Chamada do tema geral da aplicação e da estilização global

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import LandingPage from './pages/LandingPage/LandingPage'
+import { GlobalStyle } from './styles/GlobalStyle'
+import { ThemeProvider } from 'styled-components'
+import { temaGlobal } from './styles/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LandingPage />
+    <ThemeProvider theme={temaGlobal}>
+      <GlobalStyle />
+      <LandingPage />
+    </ThemeProvider>
   </React.StrictMode>,
 )
