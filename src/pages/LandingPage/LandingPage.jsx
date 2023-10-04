b37265270df521bd1f51f805506ecd8dc60d52d0
Thzzao <thiago.santos1160@gmail.com>
2023-10-04 19:43:16 -0300

    refactor: Mudança na importação dos componentes

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,14 +1,17 @@
 // import React from 'react'
-import '../../styles/globalStyle.module.css'
 import styles from './LandingPage.module.css'
 import Header from '../../components/Header/Header'
-import BannerBemVindo from '../../components/BannerBemVIndo/BannerBemVindo'
 import BannerDesafioFinanceiro from '../../components/BannerDesafioFinanceiro/BannerDesafioFinanceiro'
+import Button from '../../components/Button/Button'
+import BannerBemVindo from '../../components/BannerBemVIndo/BannerBemVindo'
+
+
 
 const LandingPage = () => {
     return (
         <div className={styles.container}>
             <Header />
+            <Button />
             <BannerBemVindo />
             <BannerDesafioFinanceiro />
         </div>
