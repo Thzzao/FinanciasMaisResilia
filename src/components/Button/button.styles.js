f50fbd36b85a98209216a84fee1498533356caee
Thzzao <thiago.santos1160@gmail.com>
2023-10-04 19:43:38 -0300

    feat: Criação do botão

---
+++ src/components/Button/button.styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const StylesButton = styled.button`
+background-color: ${(props) => props.theme.blue700};
+padding: 12px 24px;
+outline: none;
+color: ${props => props.theme.white100}
+
+
+` 
