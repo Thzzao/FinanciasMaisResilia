f9b793a684b7d078e854fa6c0585de96d78734a7
Thzzao <thiago.santos1160@gmail.com>
2023-10-09 12:02:22 -0300

    feat: Criação da página de transações

---
+++ src/pages/Transacoes/Transacoes.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+import { useParams } from "react-router-dom"
+
+const Transacoes = () => {
+    const params = useParams()
+
+    return (
+        <div>
+            <h1></h1>
+        </div>
+    )
+}
+
+export default Transacoes
