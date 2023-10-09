18d7b2a2613b588a250129db7b5255c573572cc9
Thzzao <thiago.santos1160@gmail.com>
2023-10-09 17:36:39 -0300

    refactor: Implementando requisição por id

--- src/pages/Transacoes/Transacoes.jsx
+++ src/pages/Transacoes/Transacoes.jsx
@@ -5,9 +5,11 @@ import { useParams } from "react-router-dom"
 const Transacoes = () => {
     const params = useParams()
 
+    console.log(params)
+
     return (
         <div>
-            <h1></h1>
+            <h1>{params.id}</h1>
         </div>
     )
 }
