44be85ec13fd35eccc6e5b11856c2764a7e94abc
Thzzao <thiago.santos1160@gmail.com>
2023-10-09 12:01:54 -0300

    feat: Criação da página de login

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+
+import { Link } from "react-router-dom"
+
+const Login = () => {
+    return (
+        <div>
+            Login
+            <li><Link className={'link'} to='/dashboard'>Dashboard</Link></li>
+        </div>
+
+    )
+}
+
+export default Login
+
+// ESTRUTURA HTML
