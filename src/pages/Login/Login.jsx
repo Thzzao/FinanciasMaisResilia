f6c2b7e3f2fd89b224faf83854776a4132f45be4
Thzzao <thiago.santos1160@gmail.com>
2023-10-09 17:35:34 -0300

    feat: Criação do formulário de login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,17 +1,35 @@
 // import React from 'react'
 
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
+import Logo from "../../components/common/Logo/Logo"
+import Button from "../../components/common/Button/Button"
+import login1 from "/login 1.svg"
+import { StylesBody } from "./login.styles"
 
 const Login = () => {
+    const navigate = useNavigate()
+    const handleClick = () => {
+        navigate('/')
+    }
     return (
-        <div>
-            Login
-            <li><Link className={'link'} to='/dashboard'>Dashboard</Link></li>
-        </div>
-
+        <StylesBody className="body">
+            {/* <Link className={'link'} to='/dashboard'>Dashboard</Link> */}
+            <div className="container">
+                <form className="formulario">
+                    <Logo variant='Default' />
+                    <label>Nome de usuário</label>
+                    <input type="text" id="email" placeholder="email@email.com"></input>
+                    <label>Senha</label>
+                    <input type="password" id="senha" placeholder="*********"></input>
+                    <a>Ainda não tem conta?<Link className={'link'} to='/cadastro'>Cadastre-se</Link></a>
+                    <Button texto='Entrar' variant='primary' onClick={handleClick} />
+                </form>
+                <div className="imagem">
+                    <img src={login1} alt='Vetor de um homem concluíndo tarefas com um celular atrás'></img>
+                </div>
+            </div>
+        </StylesBody>
     )
 }
 
 export default Login
-
-// ESTRUTURA HTML
