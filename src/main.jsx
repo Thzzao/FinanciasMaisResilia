aad0c024bd924344bfe13129b9ae5aa0eea42974
Thzzao <thiago.santos1160@gmail.com>
2023-10-09 11:55:25 -0300

    refactor: Importação das rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LandingPage from './pages/LandingPage/LandingPage'
-import { GlobalStyle } from './styles/GlobalStyle'
-import { ThemeProvider } from 'styled-components'
-import { temaGlobal } from './styles/theme'
+// import LandingPage from './pages/LandingPage/LandingPage'
+// import { GlobalStyle } from './styles/GlobalStyle'
+// import { ThemeProvider } from 'styled-components'
+// import { temaGlobal } from './styles/theme'
+import Rotas from './Rotas'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider theme={temaGlobal}>
-      <GlobalStyle />
-      <LandingPage />
-    </ThemeProvider>
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Rotas />
+    </React.StrictMode>,
 )
