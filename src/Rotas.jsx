d8aa9cf0f0312e149a2c2357318c49f4bea01ec9
Thzzao <thiago.santos1160@gmail.com>
2023-10-09 11:55:53 -0300

    feat: Criação das rotas no projeto

---
+++ src/Rotas.jsx
@@ -0,0 +1,28 @@
+// import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/GlobalStyle'
+import { temaGlobal } from './styles/theme'
+import LandingPage from './pages/LandingPage/LandingPage'
+import Cadastro from './pages/Cadastro/Cadastro'
+import Login from './pages/Login/Login'
+import Dashboard from './pages/Dashboard/Dashboard'
+
+const Rotas = () => {
+    return (
+        <BrowserRouter>
+            <ThemeProvider theme={temaGlobal}>
+                <GlobalStyle />
+                <Routes>
+                    <Route path='/' element={<LandingPage />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/cadastro' element={<Cadastro />} />
+                    <Route path='/dashboard' element={<Dashboard />} />
+                    <Route path='/transacoes' element={<Dashboard />} />
+                </Routes>
+            </ThemeProvider>
+        </BrowserRouter>
+    )
+}
+
+export default Rotas
