c2839e398420f7849ab0a1e0d9990b271cdc100a
Thzzao <thiago.santos1160@gmail.com>
2023-10-11 11:23:37 -0300

    feat: Criação do layout

---
+++ src/components/shared/Layout/Layout.jsx
@@ -0,0 +1,30 @@
+// import React from 'react'
+import { Link } from 'react-router-dom'
+import { StylesLayout } from './Layout.styles'
+import Logo from '../../common/Logo/Logo'
+
+const Layout = ({ children }) => {
+    return (
+        <StylesLayout>
+            <aside className='barra-lateral'>
+                <Logo />
+                <ul>
+                    <li><Link to='/dashboard'>Dasboard</Link></li>
+                    <li><Link to='/transacoes/entradas'>Entradas</Link></li>
+                    <li><Link to='/transacoes/saidas'>Saídas</Link></li>
+                </ul>
+            </aside>
+            <div>
+                <header className='header-layout'>
+                    <p>Olá, usuário</p>
+                    <Link>Sair</Link>
+                </header>
+                <main>
+                    {children}
+                </main>
+            </div>
+        </StylesLayout>
+    )
+}
+
+export default Layout
