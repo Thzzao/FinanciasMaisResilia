1d2b7a76b24e8a932599383b0a8b2ceea0b5f4f1
Thzzao <thiago.santos1160@gmail.com>
2023-10-11 11:24:31 -0300

    feat: Criação do footer

---
+++ src/components/views/LandingPage/Footer/Footer.jsx
@@ -0,0 +1,58 @@
+
+import {
+    EnvelopeSimple,
+    FacebookLogo,
+    InstagramLogo,
+    LinkedinLogo,
+    PhoneCall,
+} from "@phosphor-icons/react";
+import { temaGlobal } from "../../../../styles/theme";
+import { StyleFooter, StyleItemlist, StyleListFooter } from "./Footer.styles";
+
+const Footer = () => {
+    return (
+        <StyleFooter>
+            <div className="content">
+                <section>
+                    <StyleListFooter>
+                        <StyleItemlist>
+                        </StyleItemlist>
+                        <StyleItemlist className="icon">
+                            <EnvelopeSimple color={temaGlobal.white50} size={20} weight="fill" />
+                            financasmais@gmail.com
+                        </StyleItemlist>
+                        <StyleItemlist className="icon">
+                            <PhoneCall color={temaGlobal.white50} size={20} weight="fill" />
+                            0800 987 6543
+                        </StyleItemlist>
+                    </StyleListFooter>
+                </section>
+
+                <section>
+                    <StyleListFooter>
+                        <StyleItemlist>Sobre nós</StyleItemlist>
+                        <StyleItemlist>perguntas Frequentes</StyleItemlist>
+                        <StyleItemlist>Política de Privacidade</StyleItemlist>
+                        <StyleItemlist>Termos de Uso</StyleItemlist>
+                        <StyleItemlist>Carreiras</StyleItemlist>
+                    </StyleListFooter>
+                </section>
+                <section>
+                    <StyleListFooter className="socials">
+                        <StyleItemlist className="social">
+                            <FacebookLogo weight="thin" size={30} color={temaGlobal.white50} />
+                        </StyleItemlist>
+                        <StyleItemlist className="social">
+                            <InstagramLogo weight="thin" size={30} color={temaGlobal.white50} />
+                        </StyleItemlist>
+                        <StyleItemlist className="social">
+                            <LinkedinLogo weight="thin" size={30} color={temaGlobal.white50} />
+                        </StyleItemlist>
+                    </StyleListFooter>
+                </section>
+            </div>
+        </StyleFooter>
+    );
+};
+
+export default Footer; 
