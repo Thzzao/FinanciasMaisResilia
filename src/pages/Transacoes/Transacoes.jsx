9e64c80e5be65b0efc998f2c7ab58c74028dc884
Thzzao <thiago.santos1160@gmail.com>
2023-10-11 12:02:25 -0300

    feat: Criação das constantes das transações

--- src/pages/Transacoes/Transacoes.jsx
+++ src/pages/Transacoes/Transacoes.jsx
@@ -1,17 +1,24 @@
-// import React from 'react'
-
-import { useParams } from "react-router-dom"
+import { useParams } from 'react-router-dom'
+import Transacao from '../../components/views/Dashboard/Transacao/Transacao'
+import Layout from '../../components/shared/Layout/Layout'
+import { dataTransacoes } from '../../core/data'
 
 const Transacoes = () => {
     const params = useParams()
+    const { usuarios: [usuarioUm] } = dataTransacoes
+    const transacoes = usuarioUm.transacoes
+    const transacao = {
+        id: 'dasdasdasd',
+        data: '11/10/2023',
+        categoria: 'Salario',
+        valor: 10000,
+        tipo: 'saida'
+    }
+
 
     console.log(params)
 
-    return (
-        <div>
-            <h1>{params.id}</h1>
-        </div>
-    )
+
 }
 
 export default Transacoes
