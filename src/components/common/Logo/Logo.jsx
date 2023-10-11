5576bab2ac6d7fc4b5663001d4001e4a6a6f3f8e
Thzzao <thiago.santos1160@gmail.com>
2023-10-11 11:16:28 -0300

    refactor: Melhoria e criação da estilização da logo

--- src/components/common/Logo/Logo.jsx
+++ src/components/common/Logo/Logo.jsx
@@ -1,13 +1,26 @@
-// import React from 'react'
-import { StylesLogo, LightLogo } from './logo.styles'
+import styled from "styled-components";
+import { Plus } from "@phosphor-icons/react";
+import { temaGlobal } from "../../../styles/theme";
 
-const Logo = ({ variant }) => {
+const Logo = ({ fontSize, cor }) => {
+    const corLogo = cor === "azul" ? temaGlobal.blue500 : temaGlobal.white50;
     return (
-        <>
-            {variant == 'Default' && <StylesLogo />}
-            {variant == 'Light' && <LightLogo />}
-        </>
-    )
-}
+        <StyleLogo fontSize={fontSize} color={corLogo}>
+            Finanças <Plus size={fontSize} weight="bold" color={corLogo} />
+        </StyleLogo>
+    );
+};
 
-export default Logo
+export default Logo;
+
+const StyleLogo = styled.h1`
+    display: flex;
+    gap: 8px;
+    align-items: center;
+    font-family: "Montserrat", sans-serif;
+    font-size: ${(props) => props.fontSize};
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    color: ${(props) => props.color};
+`;
