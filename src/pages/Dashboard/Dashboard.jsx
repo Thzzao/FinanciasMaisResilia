7c7b07018505cce81465c80c6d0841b51697bbf7
Thzzao <thiago.santos1160@gmail.com>
2023-10-11 11:55:10 -0300

    refactor: Criação de uma transação para teste

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,16 +1,30 @@
-// import React from 'react'
-
 import Layout from "../../components/shared/Layout/Layout"
-
-
+import Transacao from '../../components/views/Dashboard/Transacao/Transacao'
 
 const Dashboard = () => {
+
+    const transacao = {
+        id: 'dasdasdasd',
+        data: '11/10/2023',
+        categoria: 'Salario',
+        valor: 10000,
+        tipo: 'saida'
+    }
+
     return (
         <div>
-            <Layout>
-                <p>Salvee</p>
+            <Layout >
+                <Transacao
+                    id={transacao.id}
+                    data={transacao.data}
+                    categoria={transacao.categoria}
+                    valor={transacao.valor}
+                    tipo={transacao.tipo}
+                />
             </Layout>
+
         </div>
+
     )
 }
 
