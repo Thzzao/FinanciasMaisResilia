f1d9daef6fd9041a29e922848bc7ab76b5089338
Thzzao <thiago.santos1160@gmail.com>
2023-10-18 18:10:19 -0300

    feat: Criação da lógica do componente de cada transação

--- src/components/views/Dashboard/Transacao/Transacao.jsx
+++ src/components/views/Dashboard/Transacao/Transacao.jsx
@@ -1,8 +1,39 @@
+/* eslint-disable react/prop-types */
+import { ArrowLineUp, ArrowLineDown } from '@phosphor-icons/react'
+import styled, { useTheme } from 'styled-components'
 
-const Transacao = () => {
+const Transacao = ({ id, valor, categoria, descricao, data, tipo, handleEditarTransacao }) => {
+
+    const theme = useTheme()
+
     return (
-        <div>Transacao</div>
+        <StylesTransacao>
+            {
+                tipo === 'entrada' ? <ArrowLineUp size={32} color={theme.green500} /> : <ArrowLineDown size={32} color={theme.red500} />
+            }
+            <p className='valor'>{valor}</p>
+            <p>{categoria}</p>
+            <p>{data}</p>
+            <button onClick={() => {
+                handleEditarTransacao({ id, valor, categoria, descricao, data, tipo })
+            }}>Editar</button>
+        </StylesTransacao>
     )
 }
 
 export default Transacao
+
+const StylesTransacao = styled.li`
+    width: 100%;
+    padding: 12px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    box-shadow: 4px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    border-radius: 8px;
+    background-color: ${props => props.theme.white50};
+
+    > .valor{
+        font-weight: bold;
+    }
+`
