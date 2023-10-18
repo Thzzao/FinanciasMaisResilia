e2089ddb46ae32763ac8ec858373aec15039f2b9
Thzzao <thiago.santos1160@gmail.com>
2023-10-18 18:04:59 -0300

    style: Remoção de espaçamento e melhoria na ordem dos elementos no código

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -6,10 +6,10 @@ export const temaGlobal = {
     blue100: '#D0E8E8',
     white50: '#FFFFFF',
     white200: '#E9F0F0',
+    red700: '#A3151A',
     red500: '#9A4A4D',
     red50: '#DDBDBE',
-    red700: '#A3151A',
+    green700: '#068B24',
     green500: '#34673F',
     green50: '#E8F3EB',
-    green700: '#068B24',
 }
