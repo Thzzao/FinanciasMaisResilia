24b9febbedadbad52548278dc3d9727b9e82f714
Thzzao <thiago.santos1160@gmail.com>
2023-10-18 18:05:57 -0300

    refactor: Melhoria na estilização geral do sistema

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,55 +1,85 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
-*{
+    * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family:  'Roboto', sans-serif;
+    color: ${(props) => props.theme.blue900};
 }
-body{
+
+html,
+:root {
     width: 100%;
     min-height: 100vh;
 }
-h2-Title1{
-    font-family: Montserrat;
+
+button,
+a {
+    cursor: pointer;
+}
+
+a, li{
+    text-decoration: none;
+    list-style: none;
+    color: inherit;
+}
+
+button,
+input {
+    outline: none;
+}
+
+.container{
+    width: 100%;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+}
+
+.content{
+    width: 100%;
+    max-width: 1200px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 24px 0;
+}
+
+
+
+h2{  
+    font-family: 'Montserrat';
     font-size: 38px;
     font-style: normal;
     font-weight: 700;
     line-height: normal;
 }
-h2-Title2{
-    font-family: Montserrat;
-    font-size: 32px;
-    font-style: normal;
-    font-weight: 700;
-    line-height: normal;
-}
+
 h3{
-    font-family: Roboto;
+    font-family: 'Roboto';
     font-size: 24px;
     font-style: normal;
     font-weight: 400;
     line-height: normal;
 }
-p-large{
-    font-family: Roboto;
+
+p{
+    font-family: 'Roboto';
     font-size: 24px;
     font-style: normal;
     font-weight: 300;
     line-height: normal;
 }
-a-link1{
-    font-family: Montserrat;
+
+a{
+    font-family: 'Montserrat';
     font-size: 24px;
     font-style: normal;
     font-weight: 400;
     line-height: normal;
 }
-a-link2{
-    font-family: Roboto;
-    font-size: 24px;
-    font-style: normal;
-    font-weight: 500;
-    line-height: normal;
-}
-` 
+`
