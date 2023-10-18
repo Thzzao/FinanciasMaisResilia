3b0e1e6e1966851e660e9eec45e145097d70c342
Thzzao <thiago.santos1160@gmail.com>
2023-10-18 18:14:26 -0300

    style: Remoção de espaçamento no código e importações desnecessárias

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,14 +1,12 @@
-// import React from 'react'
 import styles from '../LandingPage/landingPage.module.css'
 import Header from '../../components/views/LandingPage/Header/Header'
-import BannerDesafioFinanceiro from '../../components/views/LandingPage/BannerDesafioFinanceiro/BannerDesafioFinanceiro'
 import BannerBemVindo from '../../components/views/LandingPage/BannerBemVindo/BannerBemVindo'
+import BannerDesafioFinanceiro from '../../components/views/LandingPage/BannerDesafioFinanceiro/BannerDesafioFinanceiro'
 import Footer from '../../components/views/LandingPage/Footer/Footer'
-// import Logo from '../../components/common/Logo/Logo'
-
-
+import { getCategorias } from '../../service/api'
 
 const LandingPage = () => {
+    getCategorias('/categorias')
     return (
         <div className={styles.container}>
             <Header />
