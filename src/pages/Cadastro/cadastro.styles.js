b53aa7a6253f77e802ed98f92164e5e3926c7a3e
Thzzao <thiago.santos1160@gmail.com>
2023-10-18 18:13:54 -0300

    refactor: Separação da estilização geral e da página de cadastro

--- src/pages/Cadastro/cadastro.styles.js
+++ src/pages/Cadastro/cadastro.styles.js
@@ -1,142 +1,50 @@
-import styled from "styled-components"
-
-export const StylesContainer = styled.div`
-display: flex;
-width: 1440px;
-height: 1024px;
-justify-content: space-between;
-align-items: center;
-background-color: ${(props) => props.theme.blue500};
-
-.imagem{
-    width: 678px;
-    height: 698px;
-    flex-shrink: 0;
-}
-
-.formulario{
+import styled from "styled-components";
+export const StyleContainerCadastro = styled.main`
+    width: 100%;
+    height: 100vh;
     display: flex;
-    width: 661px;
-    padding: 48px;
-    flex-direction: column;
+    align-items: center;
     justify-content: space-between;
+    background: ${(p) => p.theme.blue500};
+
+    > section {
+    width: 50%;
+    display: flex;
+    flex-direction: column;
     align-items: center;
-    flex-shrink: 0;
-    align-self: stretch;
-    border-radius: 16px 0px 0px 16px;
-    background: ${(props) => props.theme.white200};
-    box-shadow: -4px -4px 8px 0px rgba(0, 0, 0, 0.25), 4px 4px 8px 0px rgba(0, 0, 0, 0.25);
-}
-
-form{
-    /* width: 565px;
-    height: 612px; */
-}
-
-header{
-    display: inline-flex;
-    padding: 19px 0px;
-    justify-content: center;
-    align-items: center;
-    gap: 12px;
-}
-h2{
-    display: flex;
-    width: 166px;
-    height: 46px;
-    flex-direction: column;
-    justify-content: center;
-    color: ${(props) => props.theme.blue500};
-    /* CAHAMR H2 TITULO 2 */
-}
-
-.inputNome{
-    display: inline-flex;
-    align-items: center;
-    gap: 24px;
-}
-
-.nome{
-    display: flex;
-    padding: 6px;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 12px;
-    flex: 1 0 0;
-    align-self: stretch;
-}
-
-.sobrenome{
-    display: flex;
-    padding: 6px;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 12px;
-    flex: 1 0 0;
-}
-
-.inputs{
-    display: flex;
-    width: 565px;
-    padding: 6px;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 12px;
-}
-
-label{
-    display: flex;
-    width: 195px;
-    height: 31px;
-    flex-direction: column;
-    justify-content: center;
-    /* Chamar P larg  */
-}
-
-input{
-    display: flex;
-    padding: 8px 12px;
-    align-items: flex-start;
-    gap: 10px;
-    align-self: stretch;
-    border-radius: 8px;
-    background: ${(props) => props.theme.white50};
-    box-shadow: 4px 4px 4px 0px rgba(0, 0, 0, 0.25);
     }
 
-.login{
+    form {
+    width: 35%;
+    height: 100%;
+    border-radius: 8px 0 0 8px;
+    background: ${(p) => p.theme.white200};
+    box-shadow: -4px -4px 8px 0px rgba(0, 0, 0, 0.25),
+        4px 4px 8px 0px rgba(0, 0, 0, 0.25);
     display: flex;
-    width: 547px;
+    padding: 48px;
+    flex-direction: column;
+    justify-content: center;
     align-items: center;
-}
+    gap: 24px;
 
-a{
-    display: flex;
-    width: 156px;
-    height: 38px;
-    flex-direction: column;
-    justify-content: center;
-    flex-shrink: 0;
-}
+    p{
+        display: flex;
+        gap: 5px;
+    }
 
-.link{
-    /* Chamar A - Link */
-    display: flex;
-    width: 121px;
-    height: 31px;
-    flex-direction: column;
-    justify-content: center;
-    flex-shrink: 0;
-    color:${(props) => props.theme.blue700};
-}
+    h2 {
+        color: ${(p) => p.theme.blue500};
+    }
 
-.nome{
-    display: flex;
-    padding: 6px;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 12px;
-    flex: 1 0 0;
-    align-self: stretch;
-}
+    > div {
+        width: 100%;
+        display: flex;
+        gap: 24px;
+    }
+
+    .destaque {
+        font-weight: bold;
+    }
+    }
 `
