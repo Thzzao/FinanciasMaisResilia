1037d1f7f4507586a9a6de6746ee5e892c4d104b
Thzzao <thiago.santos1160@gmail.com>
2023-10-18 17:54:12 -0300

    docs: Comentário de explicação

--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -27,3 +27,11 @@ const Rotas = () => {
 }
 
 export default Rotas
+
+//Criar o arquivo e o componente rotas
+//importar o BR -  Pai da aplicação
+//importa Routes -  define o que é dinamico e o que é estatico
+//Criar as paginas que serão utilizadas
+//Importar o Route -  define o path(caminho) de cada pagina
+//Importar Themeprovider, tema, e o globalStyle
+//Importar o componente rotas na raiz do projeto, main.jsx  
