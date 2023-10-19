f23c30328d48405d4f5260115dbed4ac2ba96c09
Thzzao <thiago.santos1160@gmail.com>
2023-10-19 00:47:58 -0300

    feat: Importação dos novos banners

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -4,6 +4,8 @@ import BannerBemVindo from '../../components/views/LandingPage/BannerBemVindo/Ba
 import BannerDesafioFinanceiro from '../../components/views/LandingPage/BannerDesafioFinanceiro/BannerDesafioFinanceiro'
 import Footer from '../../components/views/LandingPage/Footer/Footer'
 import { getCategorias } from '../../service/api'
+import BannerComeceAgora from '../../components/views/LandingPage/BannerComeceAgora/BannerComeceAgora'
+import BannerDepoimentos from '../../components/views/LandingPage/BannerDepoimentos/BannerDepoimentos'
 
 const LandingPage = () => {
     getCategorias('/categorias')
@@ -12,6 +14,10 @@ const LandingPage = () => {
             <Header />
             <BannerBemVindo />
             <BannerDesafioFinanceiro />
+            <BannerComeceAgora />
+            <BannerDepoimentos />
+            {/* Terminar o BannerDepoimentos */}
+            {/* Fazer o BannerVantangens*/}
             <Footer />
         </div>
     )
