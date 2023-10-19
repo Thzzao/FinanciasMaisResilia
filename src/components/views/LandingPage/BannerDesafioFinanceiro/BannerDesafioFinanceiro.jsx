1c7de0fb8359e2580fa8996a3095ee65ecd952e3
Thzzao <thiago.santos1160@gmail.com>
2023-10-19 01:05:50 -0300

    fix: Arrumando importação

--- src/components/views/LandingPage/BannerDesafioFinanceiro/BannerDesafioFinanceiro.jsx
+++ src/components/views/LandingPage/BannerDesafioFinanceiro/BannerDesafioFinanceiro.jsx
@@ -1,34 +1,34 @@
 import banner2 from "/LP-banner2 3.svg";
-import { StyledContainer } from "./BannerDesafioFinanceiro.styles";
+import { StyledContainer } from "./bannerDesafioFinanceiro.styles";
 
 const BannerDesafioFinanceiro = () => {
     return (
         <StyledContainer>
-            <div className={`content `}>
-                <picture>
-                    <img src={banner2} alt="" />
-                </picture>
+            <div className={`content `}></div>
+            <picture>
+                <img src={banner2} alt="" />
+            </picture>
 
-                <div className={"textos"}>
-                    <h2>O desafio Financeiro</h2>
-                    <h3>
-                        Sabemos que lidar com finanças pode ser um desafio para muitas
-                        pessoas.
-                    </h3>
-                    <p>
-                        {" "}
-                        A falta de organização, o desconhecimento das despesas e o medo de
-                        não conseguir atingir suas metas financeiras são obstáculos comuns
-                        enfrentados por todos nós.{" "}
-                    </p>
-                    <p>
-                        É por isso que desenvolvemos o Finanças+ - para ajudá-lo a superar
-                        esses desafios e alcançar a liberdade financeira que você tanto
-                        deseja.
-                    </p>
-                </div>
+            <div className={"textos"}>
+                <h2>O desafio Financeiro</h2>
+                <h3>
+                    Sabemos que lidar com finanças pode ser um desafio para muitas
+                    pessoas.
+                </h3>
+                <p>
+                    {" "}
+                    A falta de organização, o desconhecimento das despesas e o medo de
+                    não conseguir atingir suas metas financeiras são obstáculos comuns
+                    enfrentados por todos nós.{" "}
+                </p>
+                <p>
+                    É por isso que desenvolvemos o Finanças+ - para ajudá-lo a superar
+                    esses desafios e alcançar a liberdade financeira que você tanto
+                    deseja.
+                </p>
             </div>
-        </StyledContainer>
+        </div>
+        </StyledContainer >
     );
 };
 
