4f572784d321a4192781bee9aca3b9b9e4c361e3
Thzzao <thiago.santos1160@gmail.com>
2023-10-19 00:45:38 -0300

    style: Removendo espaçamentos no código

--- src/components/views/LandingPage/Footer/Footer.styles.js
+++ src/components/views/LandingPage/Footer/Footer.styles.js
@@ -11,7 +11,6 @@ export const StyleFooter = styled.footer`
         align-items: flex-start;
         padding: 48px 0;
     }
-
     > section {
         height: 100%;
         padding: 12px;
@@ -24,12 +23,13 @@ export const StyleListFooter = styled.ul`
     align-items: flex-start;
     gap: 24px;
     padding: 0px 15px;
+
     &.socials {
         flex-direction: row;
         align-items: center;
         justify-content: center;
     }
-    `;
+    `
 
 export const StyleItemlist = styled.li`
     color: ${(p) => p.theme.white50};
@@ -51,8 +51,7 @@ export const StyleItemlist = styled.li`
         background-color: ${(p) => p.theme.blue500};
         }
     }
-
     &:hover {
         font-weight: bold;
     }
-    `; 
+    `
