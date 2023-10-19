b0be1d6b5bbdb9fcf049486984264cbc66dcb7f2
Thzzao <thiago.santos1160@gmail.com>
2023-10-19 00:44:43 -0300

    refactor: Mudando o as classes para estilização

--- src/components/views/LandingPage/BannerDesafioFinanceiro/BannerDesafioFinanceiro.jsx
+++ src/components/views/LandingPage/BannerDesafioFinanceiro/BannerDesafioFinanceiro.jsx
@@ -1,26 +1,35 @@
-import banner2 from '/LP-banner2 3.svg'
-import { StylesBannerDesafioFinanceiro } from './bannerDesafioFinanceiro.styles'
+import banner2 from "/LP-banner2 3.svg";
+import { StyledContainer } from "./BannerDesafioFinanceiro.styles";
 
 const BannerDesafioFinanceiro = () => {
     return (
-        <StylesBannerDesafioFinanceiro className={'container'}>
-            <div className={'imgem'}>
-                <img src={banner2} alt=''></img>
-            </div>
-            <div className={'texto'}>
-                <h1 className={'titulo'}>O desafio Financeiro</h1>
-                <p className={'paragrafo1'}>
-                    Sabemos que lidar com finanças pode ser um desafio para muitas pessoas.
-                </p>
-                <p className={'paragrafo2e3'}>
-                    A falta de organização, o desconhecimento das despesas e o medo de não conseguir atingir suas metas financeiras são obstáculos comuns enfrentados por todos nós.
-                </p>
-                <p className={'paragrafo2e3'}>
-                    É por isso que desenvolvemos o Finanças+ - para ajudá-lo a superar esses desafios e alcançar a liberdade financeira que você tanto deseja.
-                </p>
-            </div>
-        </StylesBannerDesafioFinanceiro>
-    )
-}
+        <StyledContainer>
+            <div className={`content `}>
+                <picture>
+                    <img src={banner2} alt="" />
+                </picture>
 
-export default BannerDesafioFinanceiro
+                <div className={"textos"}>
+                    <h2>O desafio Financeiro</h2>
+                    <h3>
+                        Sabemos que lidar com finanças pode ser um desafio para muitas
+                        pessoas.
+                    </h3>
+                    <p>
+                        {" "}
+                        A falta de organização, o desconhecimento das despesas e o medo de
+                        não conseguir atingir suas metas financeiras são obstáculos comuns
+                        enfrentados por todos nós.{" "}
+                    </p>
+                    <p>
+                        É por isso que desenvolvemos o Finanças+ - para ajudá-lo a superar
+                        esses desafios e alcançar a liberdade financeira que você tanto
+                        deseja.
+                    </p>
+                </div>
+            </div>
+        </StyledContainer>
+    );
+};
+
+export default BannerDesafioFinanceiro;
--- src/components/views/LandingPage/BannerDesafioFinanceiro/bannerDesafioFinanceiro.styles.js
+++ src/components/views/LandingPage/BannerDesafioFinanceiro/bannerDesafioFinanceiro.styles.js
@@ -1,52 +1,19 @@
-import styled from "styled-components"
+import { styled } from "styled-components";
 
-export const StylesBannerDesafioFinanceiro = styled.div`
-    display: flex;
-    /* width: 1184.07px; */
+export const StyledContainer = styled.section`
     width: 100%;
-    padding: 0px 100px;
-    justify-content: space-between;
-    align-items: flex-start;
-    background: #E9F0F0;
-
-.texto {
+    height: fit-content;
     display: flex;
-    height: 465px;
-    padding: 45px 54px;
-    flex-direction: column;
-    align-items: flex-start;
-    gap: 24px;
-}
+    align-items: center;
+    justify-content: center;
+    background: #e9f0f0;
 
-.titulo {
-    color: #1D3557;
-    font-family: Montserrat;
-    font-size: 38px;
-    font-style: normal;
-    font-weight: 700;
-    line-height: normal;
-}
-
-.paragrafo1 {
-    color: var#263238;
-    font-family: Roboto;
-    font-size: 24px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
-}
-
-.paragrafo2e3 {
-    color: #263238;
-    font-family: Roboto;
-    font-size: 24px;
-    font-style: normal;
-    font-weight: 300;
-    line-height: normal;
-}
-
-.imagem {
-    width: 500px;
-    height: 500px;
-}
+    .textos {
+        width: 50%;
+        max-width: 550px;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        gap: 30px;
+    }
 `
